46cad6a5e1fa105f2544ba9e96a7f003948dc8f3
skupta12 <tosya2001@gmail.com>
2024-01-19 19:07:03 +0200

    Final changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,8 +27,8 @@ export function formatPrice(
 }
 
 export function constructMetadata({
-  title = 'DigitalHippo - the marketplace for digital assets',
-  description = 'DigitalHippo is an open-source marketplace for high-quality digital goods.',
+  title = 'BrandName - the marketplace for digital assets',
+  description = 'BrandName is an open-source marketplace for high-quality digital goods.',
   image = '/thumbnail.png',
   icons = '/favicon.ico',
   noIndex = false,
@@ -56,10 +56,10 @@ export function constructMetadata({
       title,
       description,
       images: [image],
-      creator: '@joshtriedcoding',
+      creator: '@skupta',
     },
     icons,
-    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    metadataBase: new URL('https://marketplace-app-production-b024.up.railway.app'),
     ...(noIndex && {
       robots: {
         index: false,
